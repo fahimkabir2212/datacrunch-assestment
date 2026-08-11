ecab1854623b12fb387e41d5d2c9f92b775bfbee
A K M Fahim Kabir <fahimkabir2213@gmail.com>
2026-08-11 15:26:10 +0600

    feat: enhance HomePage layout, add custom fonts, and update global styles

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,11 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Test heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt,
+        distinctio?
+      </p>
+    </div>
+  );
 }
